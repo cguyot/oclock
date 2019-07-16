428849d6dcd323f2e6a9498bb9759f7af4f11e6c
Christian GUYOT <cguyot@iMac-de-Christian.local>
2019-07-16 16:04:23 +0200

    Version utilisant jQuery

---
+++ jquery-version/js/converter-jquery.js
@@ -0,0 +1,147 @@
+endpoint = 'latest';
+
+/* appel de l'API uniquement au chargement de la page.
+
+Cela nous permet de ne pas trop user, la limitation du nombre de consultation.
+
+*/
+
+$.ajax({
+    url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + APIKey,
+    dataType: 'jsonp',
+    success: function (json) {
+
+        // test de la réponse de l'API
+
+        if (json.success == false) {
+            afficheErreur(json.error.code, json.error.info);
+        } else {
+            afficheConvertisseur(json.rates);
+        }
+
+    }
+});
+
+
+/* en cas de code d'erreur returnée par l'api, on l'affiche */
+
+function afficheErreur(codeErreur, info) {
+
+    $("body").toggleClass("erreur");
+
+    $("header").html(codeErreur);
+
+    $("#root").append("<h1> :( </h1>");
+
+    $("#root").append("<h2> " + info + " </h2>");
+
+}
+
+/* Si pas d'erreur... on y va gaiement */
+
+function afficheConvertisseur(tauxDeChange) {
+
+    // on met à jour le tableau des devises avec les valeurs récupérées de l'API
+    miseAjourTableau(tauxDeChange);
+
+    // on affiche les éléments de la page
+    afficheFormulaire();
+
+    // on affecte les actions aux éléments de la page
+    actionFormulaire();
+}
+
+
+
+/* Cette méthode prend en paramètre, la liste d'objet renvoyée par l'API
+
+- On parcourt ce dernier et on met à jour, la valeur du taux de notre 
+liste d'objet traduite en francais.
+
+- On se base sur le code ISO pour ce faire.
+
+*/
+
+function miseAjourTableau(tauxDeChange) {
+
+    $.each(tauxDeChange, function (key, value) {
+
+        if (key in devises) {
+            devises[key].value = value;
+        }
+
+    });
+
+}
+
+
+/* nous créons et affichons les éléments de la page */
+
+function afficheFormulaire() {
+
+    $("body").toggleClass("oclock");
+    $("header").html("Convertisseur de devises");
+
+    $("#root").append('<form id="entry">');
+    $("#root").append('</form>');
+
+    $("#entry").append('<input id="euros"  placeholder="Entrez la valeur en euros..." pattern="[0-9\\.]*">');
+    $("#entry").append('<label for="devise">Indiquez la devise </label>');
+    $("#entry").append('<input list="deviseList" type="text" id="devise" />');
+    $("#entry").append('<datalist id="deviseList">');
+
+    $.each(devises, function (key, value) {
+
+        $("#deviseList").append('<option value="' + devises[key].name + '" data-code="' + key + '"> </option>');
+
+    });
+
+    $("#entry").append('</datalist>');
+    $("#root").append('<div id="resultat"></div>');
+
+}
+
+
+/* nous ajoutons les actions et controles aux éléments injectés
+
+- on n'autorise que les chiffre et un seul point.
+- sinon on efface le champs.
+- si OK on passe au calcul du convertisseur.
+
+*/
+
+function actionFormulaire() {
+
+    $("#euros").keyup(function (onlyNumber) {
+
+        if ((onlyNumber.which > 47 && onlyNumber.which < 58) || (onlyNumber.which == 190 && ($("#euros").val().indexOf(".") == $("#euros").val().lastIndexOf(".")))) {
+            calcul();
+        }
+        else {
+            $("#euros").val('');
+        }
+
+    });
+
+    $("#devise").change(function () {
+        calcul();
+    });
+}
+
+
+/* on calcul, met en forme et nous injectons dans le DOM */
+
+function calcul() {
+
+    if ($("#devise").val() != "" && $("#euros").val() != "") {
+        //recuperation du code devise
+        code = $('#deviseList [value="' + $("#devise").val() + '"]').data('code');
+        result = $("#euros").val() * devises[code].value;
+
+        intro = '<div class="little">Pour ' + $("#euros").val() + ' €, vous avez : </div>'
+        end = '<div class="little">' + $("#devise").val() + ' </div>'
+
+        $("#resultat").html(intro + result.toFixed(2) + end);
+    }
+
+}
---
+++ jquery-version/js/iso4217.js
@@ -0,0 +1,644 @@
+/* 
+cet objet nous permet d'avoir les nom de devises en français
+
+Les valeurs sont mises à jour lors de l'appel de l'API
+
+*/
+
+
+var devises = {
+    AED: {
+        name: 'Dirham UAE',
+        value: 0
+    },
+    AFN: {
+        name: 'Afghani',
+        value: 0
+    },
+    ALL: {
+        name: 'Lek',
+        value: 0
+    },
+    AMD: {
+        name: 'Dram Armenien',
+        value: 0
+    },
+    ANG: {
+        name: 'Florin des Antilles néerlandaises',
+        value: 0
+    },
+    AOA: {
+        name: 'Kwanza',
+        value: 0
+    },
+    ARS: {
+        name: 'Peso Argentin',
+        value: 0
+    },
+    AUD: {
+        name: 'Dollars Australiens',
+        value: 0
+    },
+    AWG: {
+        name: 'Aruban Florin',
+        value: 0
+    },
+    AZN: {
+        name: 'Azerbaijanian Manat',
+        value: 0
+    },
+    BAM: {
+        name: 'Mark Convertible',
+        value: 0
+    },
+    BBD: {
+        name: 'Dollars Barbados',
+        value: 0
+    },
+    BDT: {
+        name: 'Taka',
+        value: 0
+    },
+    BGN: {
+        name: 'Lev Bulgare',
+        value: 0
+    },
+    BHD: {
+        name: 'Dinar Bahraini',
+        value: 0
+    },
+    BIF: {
+        name: 'Franc Burundi',
+        value: 0
+    },
+    BMD: {
+        name: 'Dollar Bermudien',
+        value: 0
+    },
+    BND: {
+        name: 'Brunei Dollar',
+        value: 0
+    },
+    BOB: {
+        name: 'Boliviano',
+        value: 0
+    },
+    BRL: {
+        name: 'Real Brésilien',
+        value: 0
+    },
+    BSD: {
+        name: 'Dollar Bahaméen',
+        value: 0
+    },
+    BTN: {
+        name: 'Ngultrum',
+        value: 0
+    },
+    BWP: {
+        name: 'Pula',
+        value: 0
+    },
+    BYR: {
+        name: 'Ruble Biélorusse',
+        value: 0
+    },
+    BZD: {
+        name: 'Dollar de Bélize',
+        value: 0
+    },
+    CAD: {
+        name: 'Dollar Canadien',
+        value: 0
+    },
+    CDF: {
+        name: 'Le Franc Congolais',
+        value: 0
+    },
+    CHF: {
+        name: 'Franc Suisse',
+        value: 0
+    },
+    CLF: {
+        name: 'Unidad de Fomento',
+        value: 0
+    },
+    CLP: {
+        name: 'Peso Chilien',
+        value: 0
+    },
+    CNY: {
+        name: 'Yuan Renminbi',
+        value: 0
+    },
+    COP: {
+        name: 'Peso Colombien',
+        value: 0
+    },
+    CRC: {
+        name: 'Costa Rican Colon',
+        value: 0
+    },
+    CUC: {
+        name: 'eso Convertible',
+        value: 0
+    },
+    CUP: {
+        name: 'Peso Cubain',
+        value: 0
+    },
+    CVE: {
+        name: 'Cabo Verde Escudo',
+        value: 0
+    },
+    CZK: {
+        name: 'Couronne Tchèque',
+        value: 0
+    },
+    DJF: {
+        name: 'Franc Djiboutien',
+        value: 0
+    },
+    DKK: {
+        name: 'Couronne Danoise',
+        value: 0
+    },
+    DOP: {
+        name: 'Peso Dominicain',
+        value: 0
+    },
+    DZD: {
+        name: 'Dinars algériens',
+        value: 0
+    },
+    EGP: {
+        name: 'Pound Égyptien',
+        value: 0
+    },
+    ERN: {
+        name: 'Nakfa',
+        value: 0
+    },
+    ETB: {
+        name: 'Birr Éthiopienne',
+        value: 0
+    },
+    EUR: {
+        name: 'Euro',
+        value: 0
+    },
+    FJD: {
+        name: 'Dollar des Fiji',
+        value: 0
+    },
+    FKP: {
+        name: 'Livre des Îles Malouines',
+        value: 0
+    },
+    GBP: {
+        name: 'Livre Sterlling',
+        value: 0
+    },
+    GEL: {
+        name: 'Lari',
+        value: 0
+    },
+    GHS: {
+        name: 'Cedi du Ghana',
+        value: 0
+    },
+    GIP: {
+        name: 'Pound de Gibraltar',
+        value: 0
+    },
+    GMD: {
+        name: 'Dalasi',
+        value: 0
+    },
+    GNF: {
+        name: 'Franc Guinéen',
+        value: 0
+    },
+    GTQ: {
+        name: 'Quetzal',
+        value: 0
+    },
+    GYD: {
+        name: 'Dollar guyanien',
+        value: 0
+    },
+    HKD: {
+        name: 'Dollar de Hong Kong',
+        value: 0
+    },
+    HNL: {
+        name: 'Lempira',
+        value: 0
+    },
+    HRK: {
+        name: 'Kuna',
+        value: 0
+    },
+    HTG: {
+        name: 'Gourde',
+        value: 0
+    },
+    HUF: {
+        name: 'Forint',
+        value: 0
+    },
+    IDR: {
+        name: 'Rupiah',
+        value: 0
+    },
+    ILS: {
+        name: 'Nouveau Sheqel Israélien',
+        value: 0
+    },
+    INR: {
+        name: 'Rupee Indienne',
+        value: 0
+    },
+    IQD: {
+        name: 'Dinar Iraquien',
+        value: 0
+    },
+    IRR: {
+        name: 'Rial Iranien',
+        value: 0
+    },
+    ISK: {
+        name: 'Couronne Islandaise',
+        value: 0
+    },
+    JMD: {
+        name: 'Dollars Jamaicain',
+        value: 0
+    },
+    JOD: {
+        name: 'Dinars Jordanien',
+        value: 0
+    },
+    JPY: {
+        name: 'Yen',
+        value: 0
+    },
+    KES: {
+        name: 'Shilling Kenyan',
+        value: 0
+    },
+    KGS: {
+        name: 'Som',
+        value: 0
+    },
+    KHR: {
+        name: 'Riel',
+        value: 0
+    },
+    KMF: {
+        name: 'Franc Comorien',
+        value: 0
+    },
+    KPW: {
+        name: 'Won Nord-coréen',
+        value: 0
+    },
+    KRW: {
+        name: 'Won',
+        value: 0
+    },
+    KWD: {
+        name: 'Dinar Kowaitien',
+        value: 0
+    },
+    KYD: {
+        name: 'Cayman Islands Dollar',
+        value: 0
+    },
+    KZT: {
+        name: 'Tenge',
+        value: 0
+    },
+    LAK: {
+        name: 'Kip',
+        value: 0
+    },
+    LBP: {
+        name: 'Pound Libanais',
+        value: 0
+    },
+    LKR: {
+        name: 'Rupee Sri Lankais',
+        value: 0
+    },
+    LRD: {
+        name: 'Dollar du Liberia',
+        value: 0
+    },
+    LSL: {
+        name: 'Loti',
+        value: 0
+    },
+    LYD: {
+        name: 'Dinars Libien',
+        value: 0
+    },
+    MAD: {
+        name: 'Dirham Marocain',
+        value: 0
+    },
+    MDL: {
+        name: 'Leu Moldavien',
+        value: 0
+    },
+    MGA: {
+        name: 'Malagasy Ariary',
+        value: 0
+    },
+    MKD: {
+        name: 'Denar',
+        value: 0
+    },
+    MMK: {
+        name: 'Kyat',
+        value: 0
+    },
+    MNT: {
+        name: 'Tugrik',
+        value: 0
+    },
+    MOP: {
+        name: 'Pataca',
+        value: 0
+    },
+    MRO: {
+        name: 'Ouguiya',
+        value: 0
+    },
+    MUR: {
+        name: 'Mauritius Rupee',
+        value: 0
+    },
+    MVR: {
+        name: 'Rufiyaa',
+        value: 0
+    },
+    MWK: {
+        name: 'Kwacha',
+        value: 0
+    },
+    MXN: {
+        name: 'Peso Mexicain',
+        value: 0
+    },
+    MYR: {
+        name: 'Ringgi Malaisien',
+        value: 0
+    },
+    MZN: {
+        name: 'Metical',
+        value: 0
+    },
+    NAD: {
+        name: 'Dollar Namibien',
+        value: 0
+    },
+    NGN: {
+        name: 'Naira',
+        value: 0
+    },
+    NIO: {
+        name: 'Cordoba',
+        value: 0
+    },
+    NOK: {
+        name: 'Couronne Norvégienne',
+        value: 0
+    },
+    NPR: {
+        name: 'Rupee Népalais',
+        value: 0
+    },
+    NZD: {
+        name: 'Dollar Néo-Zélandais',
+        value: 0
+    },
+    OMR: {
+        name: 'Rial Omani',
+        value: 0
+    },
+    PAB: {
+        name: 'Balboa',
+        value: 0
+    },
+    PEN: {
+        name: 'Nouveau Sol',
+        value: 0
+    },
+    PGK: {
+        name: 'Kina',
+        value: 0
+    },
+    PHP: {
+        name: 'Peso Phillipins',
+        value: 0
+    },
+    PKR: {
+        name: 'Rupee du Pakistan',
+        value: 0
+    },
+    PLN: {
+        name: 'Zloty',
+        value: 0
+    },
+    PYG: {
+        name: 'Guarani',
+        value: 0
+    },
+    QAR: {
+        name: 'Rial Qatari',
+        value: 0
+    },
+    RON: {
+        name: 'Leu Roumain',
+        value: 0
+    },
+    RSD: {
+        name: 'Dinar Serbe',
+        value: 0
+    },
+    RUB: {
+        name: 'Ruble Russe',
+        value: 0
+    },
+    RWF: {
+        name: 'Franc Rwandais',
+        value: 0
+    },
+    SAR: {
+        name: 'Riyal Saoudiens',
+        value: 0
+    },
+    SBD: {
+        name: 'Dollar des îles Solomon',
+        value: 0
+    },
+    SCR: {
+        name: 'Rupee des Seychelles',
+        value: 0
+    },
+    SDG: {
+        name: 'Livre Soudanais',
+        value: 0
+    },
+    SEK: {
+        name: 'Couronne Suédoise',
+        value: 0
+    },
+    SGD: {
+        name: 'Dollar Singaporien',
+        value: 0
+    },
+    SHP: {
+        name: 'Livre de Saint Helene',
+        value: 0
+    },
+    SLL: {
+        name: 'Leone',
+        value: 0
+    },
+    SOS: {
+        name: 'Shilling Somalien',
+        value: 0
+    },
+    SRD: {
+        name: 'Dollars du Surinam',
+        value: 0
+    },
+    STD: {
+        name: 'Dobra',
+        value: 0
+    },
+    SVC: {
+        name: 'Le Colon Salvadorien',
+        value: 0
+    },
+    SYP: {
+        name: 'Pound Syrien',
+        value: 0
+    },
+    SZL: {
+        name: 'Lilangeni',
+        value: 0
+    },
+    THB: {
+        name: 'Baht',
+        value: 0
+    },
+    TJS: {
+        name: 'Somoni',
+        value: 0
+    },
+    TMT: {
+        name: 'Turkménistan Nouveau Manat',
+        value: 0
+    },
+    TND: {
+        name: 'Dinars Tunisiens',
+        value: 0
+    },
+    TOP: {
+        name: 'Pa’anga',
+        value: 0
+    },
+    TRY: {
+        name: 'Livre Turque',
+        value: 0
+    },
+    TTD: {
+        name: 'Dollars de Trinidad et Tobago',
+        value: 0
+    },
+    TWD: {
+        name: 'Nouveau dollars Taiwanais',
+        value: 0
+    },
+    TZS: {
+        name: 'Shilling Tanzanien',
+        value: 0
+    },
+    UAH: {
+        name: 'Hryvnia',
+        value: 0
+    },
+    UGX: {
+        name: 'Shilling Ougandaisg',
+        value: 0
+    },
+    USD: {
+        name: 'Dollars US',
+        value: 0
+    },
+    UYU: {
+        name: 'Peso Uruguayen',
+        value: 0
+    },
+    UZS: {
+        name: 'Sum d\'Oubekistan',
+        value: 0
+    },
+    VEF: {
+        name: 'Bolivar',
+        value: 0
+    },
+    VND: {
+        name: 'Dong',
+        value: 0
+    },
+    VUV: {
+        name: 'Vatu',
+        value: 0
+    },
+    WST: {
+        name: 'Tala',
+        value: 0
+    },
+    XAF: {
+        name: 'CFA Franc BEAC',
+        value: 0
+    },
+    XCD: {
+        name: 'Dollar des Caraïbes orientales',
+        value: 0
+    },
+    XDR: {
+        name: 'SDR (Droit de tirage spécial)',
+        value: 0
+    },
+    XOF: {
+        name: 'CFA Franc BCEAO',
+        value: 0
+    },
+    XPF: {
+        name: 'Franc CFP',
+        value: 0
+    },
+    YER: {
+        name: 'Rial du Yemenl',
+        value: 0
+    },
+    ZAR: {
+        name: 'Rand',
+        value: 0
+    },
+    ZMW: {
+        name: 'Kwacha Zambien',
+        value: 0
+    },
+    ZWL: {
+        name: 'Dollars du Zimbabwe',
+        value: 0
+    }
+};
+
+
